1e3b66a7a11b224e2c1b68ae8d73978ff0c6118b
Kenny <kawhyte@gmail.com>
2019-10-07 15:49:19 -0700

    Updated

---
+++ src/MyInfo.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./MyInfo.css";
+
+function MyInfo(){
+return(
+<div className="MyInfo-main">
+
+<h1 > Kenny Whyte</h1>
+<p> 
+Doyle, Charles. IN: CRS reports (Library of Congress. Congressional Research Service); [Washington, D.C.] : Congressional Research Service, [2018?]- 1 online resource. Language: English, Database: FAU Libraries Catalog
+</p>
+
+<ul className="MyInfo-list">
+<li>Jamaica</li>
+<li>Cayman Island </li>
+<li>Alaska</li>
+</ul>
+
+</div>
+
+
+
+);
+
+}
+
+export default MyInfo;
